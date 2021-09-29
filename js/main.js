9e106e50f0b61fb7de67bf272086a4a21968ca4c
Roman <kntroman@gmail.com>
2021-09-29 18:56:42 +0600

    delete try-catch

--- js/main.js
+++ js/main.js
@@ -7,43 +7,34 @@ const getMinInt = function (number) {
 };
 
 const getRandomIntInclusive = function(from, to){
-  try {
-    if (from < 0) {
-      throw new Error('Диапазон не должен содержать отрицательных чисел');
-    }
-    else if (from > to) {
-      throw new Error('Число "до" должно быть больше числа "от"');
-    }
-    else if (checkRemainderOfDivision(from) && checkRemainderOfDivision(to) && getMinInt(from) === getMinInt(to)) {
-      throw new Error('Диапазон не содержит целого числа');
-    }
-    else {
-      const min = Math.ceil(from);
-      const max = Math.floor(to);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  } catch (err) {
-    return err;
+  if (from < 0) {
+    throw new Error('Диапазон не должен содержать отрицательных чисел');
+  }
+  else if (from > to) {
+    throw new Error('Число "до" должно быть больше числа "от"');
+  }
+  else if (checkRemainderOfDivision(from) && checkRemainderOfDivision(to) && getMinInt(from) === getMinInt(to)) {
+    throw new Error('Диапазон не содержит целого числа');
+  }
+  else {
+    const min = Math.ceil(from);
+    const max = Math.floor(to);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 };
 
 const getRandomFractionalNumber = function(from, to, numAfterDecimal) {
-  try {
-    if (from < 0) {
-      throw new Error('Диапазон не должен содержать отрицательных чисел');
-    }
-    else if (from > to) {
-      throw new Error('Число "до" должно быть больше числа "от"');
-    }
-    else if (numAfterDecimal < 0) {
-      throw new Error('Аргумент не может быть отрицательным');
-    }
-    else {
-      return +(Math.random() * (to - from) + from).toFixed(numAfterDecimal);
-    }
+  if (from < 0) {
+    throw new Error('Диапазон не должен содержать отрицательных чисел');
   }
-  catch (err) {
-    return err;
+  else if (from > to) {
+    throw new Error('Число "до" должно быть больше числа "от"');
+  }
+  else if (numAfterDecimal < 0) {
+    throw new Error('Аргумент не может быть отрицательным');
+  }
+  else {
+    return +(Math.random() * (to - from) + from).toFixed(numAfterDecimal);
   }
 };
 
