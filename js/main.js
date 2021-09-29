b18bab502836844666a177e5f4cc57044b79e6a4
Roman <kntroman@gmail.com>
2021-09-29 18:38:58 +0600

    fix logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const checkRemainderOfDivision = function (number) {
-  return (number % 1 === 0);
+  return (number % 1 !== 0);
 };
 
 const getMinInt = function (number) {
@@ -7,20 +7,46 @@ const getMinInt = function (number) {
 };
 
 const getRandomIntInclusive = function(from, to){
-  if (from >= 0 && to > from) {
-    if (checkRemainderOfDivision(from) || checkRemainderOfDivision(to) || getMinInt(from) < getMinInt(to)) {
+  try {
+    if (from < 0) {
+      throw new Error('Диапазон не должен содержать отрицательных чисел');
+    }
+    else if (from > to) {
+      throw new Error('Число "до" должно быть больше числа "от"');
+    }
+    else if (checkRemainderOfDivision(from) && checkRemainderOfDivision(to) && getMinInt(from) === getMinInt(to)) {
+      throw new Error('Диапазон не содержит целого числа');
+    }
+    else {
       const min = Math.ceil(from);
       const max = Math.floor(to);
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+  } catch (err) {
+    return err;
   }
-  return 'Некорректный диапазон. Диапазон должен: \n - Не содержать отрицательных чисел \n - Число "до" должно быть больше числа "от"  \n - Содержать как минимум одно целое число';
 };
 
-const getCustomRandom = function(from, to, numAfterDecimal) {
-  return (from >= 0 && to > from && numAfterDecimal >= 0) ? (Math.random() * (to - from) + from).toFixed(numAfterDecimal) : 'Проверьте правильность введенных значений. Условия: \n - Диапазон должен не содержать отрицательных чисел \n - Число "до" должно быть больше числа "от" \n - Значение количества чисел после запятой не отрицательное';
+const getRandomFractionalNumber = function(from, to, numAfterDecimal) {
+  try {
+    if (from < 0) {
+      throw new Error('Диапазон не должен содержать отрицательных чисел');
+    }
+    else if (from > to) {
+      throw new Error('Число "до" должно быть больше числа "от"');
+    }
+    else if (numAfterDecimal < 0) {
+      throw new Error('Аргумент не может быть отрицательным');
+    }
+    else {
+      return +(Math.random() * (to - from) + from).toFixed(numAfterDecimal);
+    }
+  }
+  catch (err) {
+    return err;
+  }
 };
 
 getRandomIntInclusive(1.2, 1.3);
-getCustomRandom(1.2, 1.217, -3);
+getRandomFractionalNumber(1.2, 1.217, -3);
 
